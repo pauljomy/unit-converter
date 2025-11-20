2ba258a696735e149c08f3fd696710fe7ece9edd
Paul Jomy <49817584+pauljomy@users.noreply.github.com>
2025-11-20 08:24:53 +0530

    working in .js file

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const length = document.getElementById("length");
+const volume = document.getElementById("volume");
+const mass = document.getElementById("mass");
+const userInput = document.getElementById("user-input");
+const convertBtn = document.getElementById("convert");
+
+convertBtn.addEventListener("click", () => {
+  const [meter, feet] = convertMeterToFeet(userInput.value);
+  console.log(meter, feet);
+});
+
+function convertMeterToFeet(input) {
+  input = Number(input.value);
+  const meter = input * 3.281;
+  const feet = input / 3.281;
+  return [meter, feet];
+}
