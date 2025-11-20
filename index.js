a3ca7137ea66e5d2debdddc6886222d7f8962553
Paul Jomy <123456789+pauljomy@users.noreply.github.com>
2025-11-20 13:56:38 +0530

    project completed

--- index.js
+++ index.js
@@ -11,13 +11,33 @@ const userInput = document.getElementById("user-input");
 const convertBtn = document.getElementById("convert");
 
 convertBtn.addEventListener("click", () => {
-  const [meter, feet] = convertMeterToFeet(userInput.value);
-  console.log(meter, feet);
+  const [meter, feet] = convertMeterFeet(userInput);
+  // console.log(meter.toFixed(3), feet.toFixed(3));
+  const [liters, gallon] = convertLitersGallon(userInput);
+  const [kg, pounds] = convertKgPounds(userInput);
+
+  length.textContent = `${userInput.value} meters = ${meter} feet | ${userInput.value} feet = ${feet} meters`;
+  volume.textContent = `${userInput.value} liters = ${liters} gallons | ${userInput.value} gallons = ${gallon} liters`;
+  mass.textContent = `${userInput.value} kilos = ${kg} pounds | ${userInput.value} pounds = ${pounds} kilos`;
 });
 
-function convertMeterToFeet(input) {
+function convertMeterFeet(input) {
   input = Number(input.value);
   const meter = input * 3.281;
   const feet = input / 3.281;
-  return [meter, feet];
+  return [meter.toFixed(3), feet.toFixed(3)];
+}
+
+function convertLitersGallon(input) {
+  input = Number(input.value);
+  const liters = input * 0.264;
+  const gallon = input / 0.264;
+  return [liters.toFixed(3), gallon.toFixed(3)];
+}
+
+function convertKgPounds(input) {
+  input = Number(input.value);
+  const kg = input * 2.204;
+  const pounds = input / 2.204;
+  return [kg.toFixed(3), pounds.toFixed(3)];
 }
